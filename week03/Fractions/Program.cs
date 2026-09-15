3098b970fcd0f2101faa9dc8805907e2d61e6f07
josephthomasrebelohtml <josepht.larocque@outlook.com>
2026-09-15 19:34:12 -0400

    Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction frac = new Fraction();
+        Fraction frac2 = new Fraction(5);
+        Fraction frac3 = new Fraction(3, 4);
+        Fraction frac4 = new Fraction(1, 3);
+
+        Console.WriteLine(frac.GetFractionString());
+        Console.WriteLine(frac.GetDecimalValue());
+
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
+
+        Console.WriteLine(frac4.GetFractionString());
+        Console.WriteLine(frac4.GetDecimalValue());
+
     }
 }
